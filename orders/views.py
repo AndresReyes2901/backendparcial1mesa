c9f7341eb823cb64e877ed599009e1c1794a838b
Oerlinker <oerlinker@gmail.com>
2025-04-10 18:10:27 -0400

    Deploy product 39 fix Stripe

--- orders/views.py
+++ orders/views.py
@@ -121,7 +121,7 @@ class StripeWebhookView(APIView):
     def post(self, request, *args, **kwargs):
         payload = request.body
         sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-        endpoint_secret = settings.STRIPE_SECRET_KEY
+        endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
         event = None
 
         try:
