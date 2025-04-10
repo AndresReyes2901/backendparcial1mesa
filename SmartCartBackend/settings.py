4b8a0e727eeb6b1208d5b20acdae4b9589e29bec
Oerlinker <oerlinker@gmail.com>
2025-04-09 21:56:32 -0400

    Deploy product 25 passwordchange email

--- SmartCartBackend/settings.py
+++ SmartCartBackend/settings.py
@@ -149,7 +149,7 @@ REST_FRAMEWORK = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.resend.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
