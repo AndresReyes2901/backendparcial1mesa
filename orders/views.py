7ea2e58787337d850214955cc6a9f9894844046d
Oerlinker <oerlinker@gmail.com>
2025-04-10 17:47:13 -0400

    Deploy product 36 fix Stripe

--- orders/views.py
+++ orders/views.py
@@ -2,17 +2,16 @@ import stripe
 from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from requests import Response
+from rest_framework.response import Response
 from rest_framework import viewsets, filters, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-
 from SmartCartBackend import settings
 from .permissions import IsOwnerOrAdminOrAssignedDelivery,IsCartOwner
 from .models import Order, OrderItem, OrderStatusHistory, Cart, CartItem
 from .serializers import OrderSerializer, OrderItemSerializer, CartSerializer, CartItemSerializer
-
+from stripe.error import StripeError
 
 class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
@@ -84,71 +83,35 @@ class CartItemViewSet(viewsets.ModelViewSet):
         serializer.save(cart=cart)
 
 
-
 class CheckoutView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
-        cart = get_object_or_404(Cart, user=request.user)
-        line_items = []
-
-        for item in cart.items.all():
-            line_items.append({
-                'price_data': {
-                    'currency': 'usd',
-                    'product_data': {
-                        'name': item.product.name,
-                    },
-                    'unit_amount': int(item.product.price * 100),
-                },
-                'quantity': item.quantity,
-            })
-
-        session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=line_items,
-            mode='payment',
-            success_url='https://backenddjango-production-c48c.up.railway.app/payment-success',
-            cancel_url='https://backenddjango-production-c48c.up.railway.app/payment-cancel',
-            metadata={"user_id": request.user.id}
-        )
-        return Response({'checkout_url': session.url})
-
-@method_decorator(csrf_exempt, name='dispatch')
-class StripeWebhookView(APIView):
-    def post(self, request, *args, **kwargs):
-        payload = request.body
-        sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-        endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
-        event = None
-
         try:
-            event = stripe.Webhook.construct_event(
-                payload, sig_header, endpoint_secret
-            )
-        except ValueError as e:
+            cart = get_object_or_404(Cart, user=request.user)
+            line_items = []
 
-            return Response(status=400)
-        except stripe.error.SignatureVerificationError as e:
-
-            return Response(status=400)
-
-
-        if event['type'] == 'checkout.session.completed':
-            session = event['data']['object']
-            user_id = session['metadata']['user_id']
-
-
-            cart = get_object_or_404(Cart, user_id=user_id)
-
-            order = Order.objects.create(client=cart.user, status="pending")
             for item in cart.items.all():
-                OrderItem.objects.create(
-                    order=order,
-                    product=item.product,
-                    quantity=item.quantity,
-                )
+                line_items.append({
+                    'price_data': {
+                        'currency': 'usd',
+                        'product_data': {
+                            'name': item.product.name,
+                        },
+                        'unit_amount': int(item.product.price * 100),
+                    },
+                    'quantity': item.quantity,
+                })
 
-            cart.items.all().delete()
+            session = stripe.checkout.Session.create(
+                payment_method_types=['card'],
+                line_items=line_items,
+                mode='payment',
+                success_url='https://backenddjango-production-c48c.up.railway.app/payment-success',
+                cancel_url='https://backenddjango-production-c48c.up.railway.app/payment-cancel',
+                metadata={"user_id": request.user.id}
+            )
+            return Response({'checkout_url': session.url})
 
-        return Response(status=200)
+        except StripeError as e:
+            return Response({'error': str(e)}, status=500)
