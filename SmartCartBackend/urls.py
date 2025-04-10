48e0d419bed0a277736ce8e20c53e3bbf4fc4300
Oerlinker <oerlinker@gmail.com>
2025-04-10 18:48:56 -0400

    Deploy product 42 fix Stripe

--- SmartCartBackend/urls.py
+++ SmartCartBackend/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from users.views import RolViewSet, UsuarioViewSet, CustomPasswordResetView, LogoutView, PasswordResetConfirmView, RegisterClienteView, RegisterDeliveryView
+from users.views import RolViewSet, UsuarioViewSet, CustomPasswordResetView, LogoutView, PasswordResetConfirmView, \
+    RegisterClienteView, RegisterDeliveryView
 from products.views import ProductViewSet
 from orders.views import OrderViewSet, OrderItemViewSet, CartViewSet, CartItemViewSet, CheckoutView, StripeWebhookView
 from rest_framework.authtoken.views import obtain_auth_token
@@ -22,13 +23,13 @@ checkout_router.register(r'checkout', CheckoutView, basename='checkout')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/', include(checkout_router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('api/logout/', LogoutView.as_view(), name='api_logout'),
     path('api/register-cliente/', RegisterClienteView.as_view(), name='register_cliente'),
     path('api/register-delivery/', RegisterDeliveryView.as_view(), name='register_delivery'),
     path('api/password-reset/', CustomPasswordResetView.as_view(), name='custom_password_reset'),
-    path('api/reset-password-confirm/<uid>/<token>/', PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
+    path('api/reset-password-confirm/<uid>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('api/checkout/', CheckoutView.as_view(), name='checkout'),
     path('api/stripe/webhook/', StripeWebhookView.as_view(), name='stripe-webhook'),
 ]
