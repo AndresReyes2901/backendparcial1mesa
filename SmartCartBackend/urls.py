7e4ad4f1fcb4e4d3511521e6112f1d8bdd12ec18
Oerlinker <oerlinker@gmail.com>
2025-04-11 10:18:08 -0400

    Deploy product 50 doc endpoints

--- SmartCartBackend/urls.py
+++ SmartCartBackend/urls.py
@@ -6,7 +6,9 @@ from users.views import RolViewSet, UsuarioViewSet, CustomPasswordResetView, Log
 from products.views import ProductViewSet
 from orders.views import OrderViewSet, OrderItemViewSet, CartViewSet, CartItemViewSet, CheckoutView, StripeWebhookView
 from rest_framework.authtoken.views import obtain_auth_token
-from django.contrib.auth import views as auth_views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = routers.DefaultRouter()
 router.register(r'roles', RolViewSet)
@@ -17,6 +19,19 @@ router.register(r'order-items', OrderItemViewSet, basename='orderitem')
 router.register(r'cart', CartViewSet, basename="cart")
 router.register(r'cart-items', CartItemViewSet, basename="cart-items")
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="SmartCart API",
+        default_version='v1',
+        description="Documentación de los endpoints del proyecto SmartCart",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="oerlinker@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
@@ -25,7 +40,10 @@ urlpatterns = [
     path('api/register-cliente/', RegisterClienteView.as_view(), name='register_cliente'),
     path('api/register-delivery/', RegisterDeliveryView.as_view(), name='register_delivery'),
     path('api/password-reset/', CustomPasswordResetView.as_view(), name='custom_password_reset'),
-    path('api/reset-password-confirm/<uid>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('api/reset-password-confirm/<uid>/<token>/', PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
     path('api/checkout/', CheckoutView.as_view(), name='checkout'),
     path('api/stripe/webhook/', StripeWebhookView.as_view(), name='stripe-webhook'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
