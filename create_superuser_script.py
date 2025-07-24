0e0516edc10fc181d41aed7affbcd065b5f66204
Lo Yepez <lorenaponce246@gmail.com>
2025-07-24 01:16:59 -0400

    Update create_superuser_script.py

--- create_superuser_script.py
+++ create_superuser_script.py
@@ -8,7 +8,7 @@ email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
 password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin123")
 
 if not User.objects.filter(nombre=username).exists():
-    User.objects.create_superuser(nombre=username, correo=email, password=password)
+    User.objects.create_superuser(nombre=username,apelllido=username, correo=email, password=password)
     print("✅ Superuser creado")
 else:
     print("ℹ️ El superuser ya existe")
